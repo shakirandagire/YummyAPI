80d14c63fe1c73bd056cf2ca3b38f5ef1fad74cf
Shakira <ndagire.shakira@gmail.com>
2017-12-17 14:07:44 +0300

    Update  to run.py

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from app import create_app
 from instance.config import app_config
 
 # config_name = os.getenv('APP_SETTINGS')
-config_name = "development"
+config_name = os.getenv('APP_SETTINGS')
+# config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
