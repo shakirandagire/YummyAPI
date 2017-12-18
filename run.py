599fa29d2d0ce747f8c2a26cf5f75cbad776da2a
Shakira <ndagire.shakira@gmail.com>
2017-12-18 15:10:50 +0300

    Changes to run.py file

--- run.py
+++ run.py
@@ -3,9 +3,7 @@ import os
 from app import create_app
 from instance.config import app_config
 
-# config_name = os.getenv('APP_SETTINGS')
 config_name = os.getenv('FLASK_CONFIG')
-# config_name = "development"
 app = create_app(config_name)
 if __name__ == '__main__':
     app.run()
