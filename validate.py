465f93986941b3134ff45bbbc6b3eee17f927e09
Shakira Seruwagi <shakiraseruwagi@Shakiras-MacBook-Pro.local>
2018-01-04 18:53:52 +0300

    Added changes to validate.py

--- validate.py
+++ validate.py
@@ -1,4 +1,16 @@
+
 import re
 
-def validate_categoryname(categoryname):
-    categoryname
+def valid_name(name):
+    name = str(name)
+    if re.match(r'[a-zA-Z]*$', name):
+        return True
+
+def valid_password(password):
+    if re.match(r'[a-zA-Z0-9]', password):
+        return 'Valid Password'
+       
+def valid_email(email):
+    if re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', email):
+        return 'Valid Email'
+       
